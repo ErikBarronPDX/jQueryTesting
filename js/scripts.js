39e22d813d7cda2b279d7eb99b90803132532d11
Erik Barron <todayisagrnday21@gmail.com>
2020-01-14 19:49:52 -0800

    display content for a darkMode button

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
   $("#show").click(function(){
     $(".notShown").toggle();
   })
+
+  $("#darkMode").click(function() {
+    $("body").toggleClass("darkMode");
+  });
 })
+
